810ec9cf21f6e24164566fc9415a1e110c3890d1
isfaaghyth <isfaaghyth@gmail.com>
2022-01-08 16:28:03 +0700

    final project

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * TODO 1: SETUP SERVER USING EXPRESS.JS.
+ * UBAH SERVER DI BAWAH MENGGUNAKAN EXPRESS.JS.
+ * SERVER INI DIBUAT MENGGUNAKAN NODE.JS NATIVE.
+ */
+
+(function(){"use strict";require("http").createServer(function(t,e){e.writeHead(200,{"Content-Type":"text/html"}),e.write("Final Project UAS - Good Luck."),e.end()}).listen(3e3,function(){console.log("[Server] Running at: http://localhost:3000")})}).call(this);
---
+++ config/app.js
@@ -0,0 +1,1 @@
+(function(){"use strict";var e=require("crypto"),n=require("base64url"),i=require("fs"),r=Date.now(),t=n(e.randomBytes(64));i.appendFile("./config/app.js","\n//UNIX="+r+"\n//APP_KEY="+t,function(e){if(e)throw e}),i.appendFile(".env","\n#UNIX="+r+"\n#APP_KEY="+t,function(e){if(e)throw e;process.exit(0)})}).call(this);
